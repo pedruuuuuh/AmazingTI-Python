737205c3e9fd444ee8e9f0050df0d7a25ad3d6a2
Pedro Henrique <103459126+pedruuuuuh@users.noreply.github.com>
2022-11-18 21:48:35 -0300

    Add files via upload

--- monera.py
+++ monera.py
@@ -7,15 +7,17 @@ nomes = []
 
 cnpj = []
 nomes_empresa = []
+info_empresa = []
+curtidas = []
 
 opc = ""
 
 while(prin_opc != 3):
     prin_opc = int(input(f"{'-=' * 5} Seja bem vindo(a) à Munera! {'=-' * 5}\n" +
-                          "O que voçê deseja fazer? : \n" +
                           "1 - Fazer Login/Cadastro\n" +
                           "2 - Ver empresas\n" +
-                          "3 - Sair\n"
+                          "3 - Sair\n" + 
+                          "Oque voçê deseja fazer: "
     ))
 
     print()
@@ -36,13 +38,16 @@ while(prin_opc != 3):
         while(opc != 5):
 
             print()
-            opc = int(input("Escolha uma opção : \n" +
+            opc = int(input(
             "1 - Cadastrar usuario\n" +
             "2 - Cadastrar empresa\n" + 
             "3 - Fazer login usuario\n" + 
             "4 - Fazer login empresa\n" +
-            "5 - Voltar\n" 
+            "5 - Voltar\n" +
+            "Escolha uma opção : "
             ))
+            print()
+
             if(opc == 1):
 
                 nomes.append(input("Informe o seu nome : "))
@@ -54,7 +59,9 @@ while(prin_opc != 3):
 
                 cnpj.append(input("Informe o cnpj da empresa: "))
                 nomes_empresa.append(input("Informe o nome da empresa : "))
+                info_empresa.append(input("Use esse espaço para falar mais sobre a sua empresa : "))
                 print("Cadastro realizado com sucesso!")
+                curtidas.append(0)
 
             elif(opc == 3):
 
@@ -77,8 +84,52 @@ while(prin_opc != 3):
 
                 while(res_s == False):
                     if(senha_inserida == senhas[pos]):
-                        print(f"Seja bem-vindo {nomes[pos]}")
+                        print()
+
+                        print(f"Seja bem-vindo {nomes[pos]}!")
+
+                        print(f"\n{'=-' * 7} EMPRESAS {'-=' * 7}")
+
+                        for j in range(0, len(nomes_empresa), 1):
+                            print(f"{j + 1} - {nomes_empresa[j]}")
+                            print("Sobre a empresa: ")
+                            print(info_empresa[j])
+                            print(f"CURTIDAS: {curtidas[j]}")
+                            print()
+
+                        index = ""
+                        resp_c = input("Você deseja curtir alguma empresa? [S/N] : ").upper()
+
+                        while(resp_c != 'S'):
+                            if(resp_c == 'N'):
+                                break
+                            else:
+                                print("Escolha uma opção válida!")
+                                resp_c = input("Você deseja curtir alguma empresa? [S/N] : ").upper()
+
+                        while(resp_c == 'S'): 
+                            index = int(input("Qual o numero da empresa que voçê deseja curtir? : ")) - 1
+                            curtidas[index] = curtidas[index] + 1
+
+                            print(f"\n{'=-' * 7} EMPRESAS {'-=' * 7}")
+                            for j in range(0, len(nomes_empresa), 1):
+                                print()
+                                print(f"{j + 1} - {nomes_empresa[j]}")
+                                print("Sobre a empresa: ")
+                                print(info_empresa[j])
+                                print(f"CURTIDAS: {curtidas[j]}")
+                                print()
+
+                            resp_c = input("Você deseja curtir mais alguma empresa? [S/N] : ").upper()
+                            while(resp_c != 'S'):
+                                if(resp_c == 'N'):
+                                    break
+                                else:
+                                    print("Escolha uma opção válida!")
+                                    resp_c = input("Você deseja curtir mais alguma empresa? [S/N] : ").upper()
+
                         res_s = True
+
                     else:
                         print("Senha incorreta!")
                         senha_inserida = input("Informe a sua senha : ")
@@ -114,9 +165,13 @@ while(prin_opc != 3):
     elif(prin_opc == 2):
         c = 1
         
-        print("\nEMPRESAS: ")
+        print(f"\n{'=-' * 7} EMPRESAS {'-=' * 7}")
         for j in range(0, len(nomes_empresa), 1):
             print(f"{c} - {nomes_empresa[j]}")
+            print("Sobre a empresa: ")
+            print(info_empresa[j])
+            print(f"CURTIDAS: {curtidas[j]}")
+            print()
             c =+ 1
         print()
 
